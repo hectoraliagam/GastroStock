a936ab1ddc5237c467cd0841758dfd018833a2be
hectoraliagam <aliagamdnhectorgbl@gmail.com>
2026-09-01 01:55:26 -0500

    v.0.0.9

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // const API_URL = 'http://127.0.0.1:8000/api';
 
 // MODO PRODUCCIÓN (Nube)
-const API_URL = 'https://gastrostock-27s9.onrender.com';
+const API_URL = 'https://gastrostock-27s9.onrender.com/api';
 
 const loader = document.getElementById('loading');
 const toggleLoader = (show) => loader.style.display = show ? 'flex' : 'none';
